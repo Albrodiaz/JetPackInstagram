f61876140118d44c6e8b3cfb4a0201b86166008f
Alberto Rodríguez Díaz <choketido@gmail.com>
2022-09-15 19:46:35 +0200

    Initial commit

---
+++ app/src/main/java/com/example/jetpackinstagram/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.jetpackinstagram
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetpackinstagram.login.LoginScreen
+import com.example.jetpackinstagram.login.LoginViewModel
+import com.example.jetpackinstagram.ui.theme.JetPackInstagramTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetPackInstagramTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    LoginScreen(LoginViewModel())
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetPackInstagramTheme {
+        Greeting("Android")
+    }
+}
